ebf12bd39d884f38876f0463504c117597963d20
Yabing <yabingc3@gmail.com>
2024-01-19 17:54:37 +0100

    parsing fini, reste a tester

--- includes/cub3D.h
+++ includes/cub3D.h
@@ -6,7 +6,7 @@
 /*   By: yachen <yachen@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/05 10:28:54 by yachen            #+#    #+#             */
-/*   Updated: 2024/01/16 13:35:45 by yachen           ###   ########.fr       */
+/*   Updated: 2024/01/19 17:10:33 by yachen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,7 +65,6 @@ typedef struct s_gameconfig
 	void		*mlx_w;
 
 }				t_gameconfig;
-
 
 int		parsing(char *gamefile, t_gameconfig *config);
 void	clear_list_ptr(t_list **list);
--- srcs/parsing.c
+++ srcs/parsing.c
@@ -6,7 +6,7 @@
 /*   By: yachen <yachen@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 17:11:48 by yachen            #+#    #+#             */
-/*   Updated: 2024/01/16 17:30:18 by yachen           ###   ########.fr       */
+/*   Updated: 2024/01/19 17:53:06 by yachen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,8 +66,25 @@ void	read_file_to_list(int fd, t_gameconfig *config)
 	close(fd);
 }
 
+int	check_info(char *tmp)
+{
+	int	i;
+	
+	i = 0;
+	while (tmp[i] && (tmp[i] == ' ' || tmp[i] == '\t'))
+		i++;
+	while(tmp[i] && tmp[i] != ',' && tmp[i] != '\n')
+	{
+		if (!ft_isdigit(tmp[i]))
+			return (err("Error!\nColor code has to be digit: ", tmp, "\n"));
+		i++;
+	}
+	(*tmp)[i] = '\0';
+	if (ft_atoi(tmp) < 0  || ft_atoi(tmp) > 255)
+		return (err("Error!\nColor code has to be between 0-255: ", tmp, "\n"));
+	return (0);
+}
 
-// cette fonction est peut etre a revoir a cause des white spaces
 int	with_correct_info(char *line)
 {
 	char	**tab;
@@ -80,22 +97,10 @@ int	with_correct_info(char *line)
 	tmp = tab;
 	while (*tmp)
 	{
-		if (ft_strlen(*tmp) > 4)
+		if (ft_strlen(*tmp) > 4 || check_info(*tmp) == -1)
 		{
 			free_tab(tab);
 			return (0);
-		}
-		i = 0;
-		while ((*tmp)[i] && (*tmp)[i] == ' ' || (*tmp)[i] == '\t')
-			i++;
-		while((*tmp)[i] && (*tmp)[i] != ',' && (*tmp)[i] != '\n')
-		{
-			if (!ft_isdigit((*tmp)[i]))
-			{
-				free_tab(tab);
-				return (0);
-			}
-			i++;
 		}
 		tmp++;
 	}
@@ -112,7 +117,7 @@ int	is_element(char *line)
 		|| line[0] == 'C' || line[0] == 'F') && (line[1] == ' ' || line[1] == '\t'))
 	{
 		tmp++;
-		while (*tmp && *tmp == ' ' || *tmp == '\t')
+		while (*tmp && (*tmp == ' ' || *tmp == '\t'))
 			tmp++;	
 		if (line[0] == 'N' || line[0] == 'S' || line[0] == 'W' || line[0] == 'E')
 		{
@@ -124,7 +129,7 @@ int	is_element(char *line)
 			}
 			return (1);
 		}
-		else if ((line[0] == 'C' || line[0] == 'F') && with_correct_info(tmp))
+		else if ((line[0] == 'C' || line[0] == 'F') && with_correct_info(tmp, ft_strlen(tmp)))
 			return (1);
 	}
 	return (0);
@@ -184,10 +189,99 @@ int	is_empty_line(char *line)
 	return (1);
 }
 
-int	check_wall(char **map, int size);
+int	have_right_wall(char **map, int i, int j)
 {
-	// idee : "si un 0 ou un N/S/E/W ou un 2 est en contacte 
-	// avec un espace ou le 'bord' de la map, alors la map n'est pas fermée, et il y a erreur."
+	while (map[i][j] && map[i][j] != '\n')
+	{
+		if (map[i][j] == '1')
+			return (1);
+		j++;
+	}
+	return (0);
+}
+
+int	have_left_wall(char **map, int i, int j)
+{
+	while (j >= 0)
+	{
+		if (map[i][j] == '1')
+			return (1);
+		j--;
+	}
+	return (0);
+}
+
+int	have_top_wall(char **map, int i, int j)
+{
+	while (i >= 0)
+	{
+		if (map[i][j] == '1')
+			return (1);
+		i--;
+	}
+	return (0);
+}
+
+int	have_bottom_wall(char **map, int i, int j, int size)
+{
+	while (i < size)
+	{
+		if (map[i][j] == '1')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	is_surrounded_by_walls(char **map, int size, int i, int j)
+{
+	if (have_right_wall(map, i, j) && have_left_wall(map, i, j)
+		&& have_top_wall(map, i, j) && have_bottom_wall(map, i, j, size))
+		return (1);
+	return (0);
+}
+
+int	check_wall(char **map, int size)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (map[i])
+	{
+		j = 0;
+		while (map[i][j])
+		{
+			if ((map[i][j] == '0' || map[i][j] == 'N' || map[i][j] == 'S'
+				|| map[i][j] == 'W' || map[i][j] == 'E') && (!is_surrounded_by_walls(map, size, i, j)))
+				return (err("Error!\nMap not closed in the line\n", map[i], "\n"));
+			if (map[i][j] == ' ' && is_surrounded_by_walls(map, size, i, j))
+				return (err("Error!\nSpace in map in the line\n", map[i], "\n"));
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+
+int error_cases(char **map, int row, int col, int *ply_pos)
+{
+	if (map[row][col] == '\t')
+	{
+		ft_putstr_fd("Error!\nDetecting tabulation in map part\n", 2);
+		ft_putstr_fd("Please, replace it by spaces and try again\n", 2);
+		return (-1);
+	}
+	if (map[row][col] != ' ' && map[row][col] != '1'
+		&& map[row][col] != '0' && map[row][col] != 'N' && map[row][col] != 'S'
+		&& map[row][col] != 'W' && map[row][col] != 'E')
+		return (err("Error!\nWrong map content: ", map[row], "\n"));
+	if (map[row][col] == 'N' || map[row][col] == 'S' || map[row][col] == 'W'
+		|| map[row][col] == 'E')
+		(*ply_pos)++;
+	if (*ply_pos > 1)
+		return (err("Error!\nThere is too many player in map\n", "",""));
+	return (0);
 }
 
 int	check_map(char **map, int size)
@@ -201,37 +295,38 @@ int	check_map(char **map, int size)
 	while (map[row])
 	{
 		col = 0;
-		while (map[row][col])
+		while (map[row][col] && map[row][col] != '\n')
 		{
-			if (map[row][col] != ' ' && map[row][col] != '\t' && map[row][col] != '1'
-				&& map[row][col] != '0' && map[row][col] != 'N' && map[row][col] != 'S'
-				&& map[row][col] != 'W' && map[row][col] != 'E')
-				return (err("Error!\nWrong map content: ", map[row], "\n"));
-			if (map[row][col] == 'N' || map[row][col] == 'S' || map[row][col] == 'W'
-				|| map[row][col] == 'E')
-				ply_pos++;
-			if (ply_pos != 1)
-				return (err("Error\nThere is too many player in map", "",""));
+			if (error_cases(map, row, col, &ply_pos) == -1)
+				return (-1);
 			col++;
 		}
 		row++;
 	}
+	if (ply_pos == 0)
+		return (err("Error!\nPlayer position missing\n", "",""));
 	return (check_wall(map, size));
+}
+
+int	find_map_size(t_list *start)
+{
+	int		size;
+
+	size = 0;
+	while (start)
+	{
+		start = start->next;
+		size++;
+	}
+	return (size);
 }
 
 int	make_map(t_list *start, t_gameconfig *config)
 {
 	int		i;
 	int		size;
-	t_list	*tmp;
 
-	size = 0;
-	tmp = start;
-	while (tmp)
-	{
-		tmp = tmp->next;
-		size++;
-	}
+	size = find_map_size(start);
 	config->map = (char **)malloc(sizeof(char *) * (size + 1));
 	if (!config)
 		return (err("Error!\n","Make_map: ", "Malloc failed\n"));
@@ -251,7 +346,7 @@ int	make_map(t_list *start, t_gameconfig *config)
 			start = start->next;
 	}
 	config->map[i] = NULL;
-	return (check_map(config->map, size))
+	return (check_map(config->map, size));
 }
 
 int	get_info_from_list(t_gameconfig *config)
@@ -268,11 +363,7 @@ int	get_info_from_list(t_gameconfig *config)
 		else if (is_empty_line(tmp->content))
 			rslt = 0;
 		else if (is_start_map(tmp->content) && config->nb_element == 6)
-		{
-			if (make_map(tmp, config) == 0)
-				break ;
-			rslt = -1;
-		}
+			return (make_map(tmp, config)); 
 		else
 			rslt = -1;
 		if (rslt == -1)
